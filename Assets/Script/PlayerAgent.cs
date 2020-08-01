a651627c7fe5f2e220cfcb6c89982092f73c8555
jspanos1 <69064381+jspanos1@users.noreply.github.com>
2020-08-02 01:03:00 +1000

    Initial commit

---
+++ Assets/Script/PlayerAgent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAgent : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform END;
+
+    void Start ()
+    {
+        var playerAgent = GetComponent<NavMeshAgent>();
+        playerAgent.destination = END.position;
+    }
+}
